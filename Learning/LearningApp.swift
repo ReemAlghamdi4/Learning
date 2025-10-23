b76df5d3af19b3d87e2705248f96f44238a65f6c
Reem <ReemAlghamdi4>
2025-10-23 10:47:20 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Reem alghamdi on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningTests/LearningTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearningTests.swift
+//  LearningTests
+//
+//  Created by Reem alghamdi on 01/05/1447 AH.
+//
+
+import Testing
+
+struct LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
